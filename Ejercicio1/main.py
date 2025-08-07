3f2d37153fb630ee016f3c5c1ec8717ee60d7f6d
alerverde <verde_alejandro@hotmail.com>
2025-08-07 11:45:46 -0300

    ej2

--- Ejercicio1/main.py
+++ Ejercicio1/main.py
@@ -1,10 +1,19 @@
-# main.py
 from extract import dataframes
 from load import load_all
+from schema import metadata, get_engine
+from schema import DimDate, DimCustomerSegment, DimProduct, FactSales
 
 def main():
     print("\nIniciando pipeline ETL...\n")
+    
+    # Crear el esquema si no existe
+    engine = get_engine()
+    metadata.create_all(engine)
+    print("Esquema en la base espejo creado o verificado.\n")
+
+    # Ejecutar la carga
     load_all(dataframes)
+
     print("\nPipeline ETL completado exitosamente.")
 
 if __name__ == "__main__":
